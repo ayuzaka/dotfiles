9b5daf40e02b01131e9106f09fb3fd503a3a8aa6
ayuzaka <ayuzaka.f@gmail.com>
2024-11-25 17:15:41 +0900

    copilot

--- dot_config/nvim/lua/plugins/copilot.lua
+++ dot_config/nvim/lua/plugins/copilot.lua
@@ -1,37 +1,3 @@
 vim.g.copilot_filetypes = {
   dotenv = false
 }
-
-local copilot_group = vim.api.nvim_create_augroup("disableGitHubCopilot", { clear = true })
-
-local disable_paths = {
-  "$HOME/workspace/ayuzaka/learn/*",
-  "Users/ayuzaka/Downloads/diary/*",
-  "$HOME/Documents/Obsidian Vault/*",
-}
-
---vim.api.nvim_create_autocmd({"LspAttach"}, {
---  pattern = disable_paths,
---  group = copilot_group,
---  callback = function (args)
---    if not args['data'] or not args.data['client_id'] then return end
---
---    local client = vim.lsp.get_client_by_id(args.data.client_id)
---    if client.name == 'copilot' then
---      vim.lsp.stop_client(client.id, true)
---    end
---  end
---})
-
-for _, pattern in ipairs(disable_paths) do
-  vim.api.nvim_create_autocmd("LspAttach", {
-    group = copilot_group,
-    pattern = pattern,
-    callback = vim.schedule_wrap(function(args)
-      local client = vim.lsp.get_client_by_id(args.data.client_id)
-      if client.name == "copilot" then
-        vim.cmd("Copilot detach")
-      end
-    end),
-  })
-end

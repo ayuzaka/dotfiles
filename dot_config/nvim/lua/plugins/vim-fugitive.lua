c3d7bee98ed9536b2fa4dc75cac5eec3b2aecbb1
ayuzaka <ayuzaka.f@gmail.com>
2024-12-15 14:39:35 +0900

    remove unused file

--- dot_config/nvim/lua/plugins/vim-fugitive.lua
+++
@@ -1,6 +0,0 @@
-local function git_show_current_hash()
-  local hash = vim.fn.expand("<cword>")
-  vim.cmd("Git show ".. hash)
-end
-
-vim.api.nvim_create_user_command("Gb", "Git blame", {})
--- dot_config/nvim/lua/plugins/vim-svelte.lua
+++
@@ -1,4 +0,0 @@
-vim.g.svelte_indent_script = 0
-vim.g.svelte_indent_style = 0
-
-vim.g.svelte_preprocessors = {"typescript"}

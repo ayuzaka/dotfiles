2ed9f0d48340c4f2f6ec1c7e599171b57b1c7ba2
ayuzaka <ayuzaka.f@gmail.com>
2025-01-13 21:49:18 +0900

    use jpdoc

--- dot_config/wezterm/wezterm.lua
+++ dot_config/wezterm/wezterm.lua
@@ -3,7 +3,7 @@ local keybinds = require('keybinds')
 
 return {
   color_scheme = "GruvboxDark",
-  font = wezterm.font("UDEV Gothic", { weight = 'Regular' }),
+  font = wezterm.font("UDEV Gothic JPDOC", { weight = 'Regular' }),
   use_ime = true,
   hide_tab_bar_if_only_one_tab = true,
   tab_bar_at_bottom = true,

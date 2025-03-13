e2dfe3efa8336f76038239ed06da00eaa1437a4c
ayuzaka <ayuzaka.f@gmail.com>
2025-03-13 23:35:22 +0900

    fix: AquaSKK can be converted to Japanese without line breaks in Wezterm.

--- dot_config/wezterm/wezterm.lua
+++ dot_config/wezterm/wezterm.lua
@@ -5,6 +5,7 @@ return {
   color_scheme = "GruvboxDark",
   font = wezterm.font("UDEV Gothic nf", { weight = 'Regular' }),
   use_ime = true,
+  macos_forward_to_ime_modifier_mask = "SHIFT|CTRL",
   hide_tab_bar_if_only_one_tab = true,
   tab_bar_at_bottom = true,
   show_new_tab_button_in_tab_bar = false,

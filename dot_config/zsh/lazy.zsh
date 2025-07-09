a80d1f56d143982aecfe839197037af87046d454
kazuya <kazuya@melty.co.jp>
2025-07-09 15:59:08 +0900

    use Docker Desktop

--- dot_config/zsh/lazy.zsh
+++ dot_config/zsh/lazy.zsh
@@ -93,9 +93,6 @@ source "$(brew --prefix)/share/google-cloud-sdk/completion.zsh.inc"
 
 source "$RYE_HOME/env"
 
-# Added by OrbStack: command-line tools and integration
-source $HOME/.orbstack/shell/init.zsh 2>/dev/null || :
-
 export WASMTIME_HOME="$XDG_DATA_HOME/wasmtime"
 export PATH="$WASMTIME_HOME/bin:$PATH"
 

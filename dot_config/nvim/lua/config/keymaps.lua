2a6986cdb8cb22db66df360207e47f1df5ddbf84
kazuya <40567261+ayuzaka@users.noreply.github.com>
2025-08-13 11:45:09 +0900

    chore: copy and paste in Neovide

--- dot_config/nvim/lua/config/keymaps.lua
+++ dot_config/nvim/lua/config/keymaps.lua
@@ -26,3 +26,18 @@ vim.keymap.set("x", "a`", "2i`", { silent = true })
 vim.keymap.set("o", "a\"", "2i\"", { silent = true })
 vim.keymap.set("o", "a'", "2i'", { silent = true })
 vim.keymap.set("o", "a`", "2i`", { silent = true })
+
+if vim.g.neovide then
+  vim.keymap.set('n', '<D-s>', ':w<CR>') -- Save
+  vim.keymap.set('v', '<D-c>', '"+y') -- Copy
+  vim.keymap.set('n', '<D-v>', '"+P') -- Paste normal mode
+  vim.keymap.set('v', '<D-v>', '"+P') -- Paste visual mode
+  vim.keymap.set('c', '<D-v>', '<C-R>+') -- Paste command mode
+  vim.keymap.set('i', '<D-v>', '<ESC>l"+Pli') -- Paste insert mode
+end
+
+-- Allow clipboard copy paste in neovim
+vim.api.nvim_set_keymap('', '<D-v>', '+p<CR>', { noremap = true, silent = true})
+vim.api.nvim_set_keymap('!', '<D-v>', '<C-R>+', { noremap = true, silent = true})
+vim.api.nvim_set_keymap('t', '<D-v>', '<C-R>+', { noremap = true, silent = true})
+vim.api.nvim_set_keymap('v', '<D-v>', '<C-R>+', { noremap = true, silent = true})

badda720b045e180d0a0d809e2ddd87c33e2eb13
ayuzaka <40567261+ayuzaka@users.noreply.github.com>
2025-09-27 15:35:42 +0900

    chore: switch from vim-lsp to nvim-lsp

---
+++ dot_config/nvim/lua/lsp/init.lua
@@ -0,0 +1,77 @@
+vim.diagnostic.config({
+  virtual_text = true
+})
+
+local augroup = vim.api.nvim_create_augroup("lsp/init.lua", {})
+
+vim.api.nvim_create_autocmd("LspAttach", {
+  group = augroup,
+  callback = function(args)
+    local client = assert(vim.lsp.get_client_by_id(args.data.client_id))
+
+    vim.api.nvim_set_option_value("omnifunc", "v:lua.vim.lsp.omnifunc", { buf = args.buf })
+    vim.api.nvim_set_option_value("tagfunc", "v:lua.vim.lsp.tagfunc", { buf = args.buf })
+
+    vim.keymap.set("n", "[g", function()
+      vim.diagnostic.jump({ count = -1, severity = { min = vim.diagnostic.severity.WARN }, wrap = true })
+    end, { buffer = args.buf, desc = "go to previous diagnostic" })
+
+    vim.keymap.set("n", "]g", function()
+      vim.diagnostic.jump({ count = 1, severity = { min = vim.diagnostic.severity.WARN }, wrap = true })
+    end, { buffer = args.buf, desc = "go to next diagnostic" })
+
+    vim.keymap.set("n", "<leader>rn", function()
+      vim.lsp.buf.rename()
+    end, { buffer = args.buf, desc = "vim.lsp.buf.rename()" })
+
+    vim.keymap.set("n", "<space>fmt", function()
+      vim.lsp.buf.format({ bufnr = args.buf, id = client.id })
+    end, { buffer = args.buf, desc = "Format buffer" })
+  end,
+})
+
+local lua_ls_opts = require("lsp.lua_ls")
+vim.lsp.config("lua_ls", lua_ls_opts)
+
+local vtsls_opts = require("lsp.vts_ls")
+vim.lsp.config("vtsls", vtsls_opts)
+
+vim.lsp.enable({
+  "lua_ls",
+  "gopls",
+  "vue_ls",
+  "svelte",
+  "rust_analyzer",
+  "pylsp",
+  "bashls",
+  "html",
+  "cssls",
+  "stylelint_lsp",
+  "tailwindcss",
+  "eslint",
+  "biome",
+  "gh_actions_ls",
+  "docker_language_server",
+})
+
+-- vts_ls or deno
+vim.api.nvim_create_autocmd("FileType", {
+  group = vim.api.nvim_create_augroup("LspStartNodeOrDeno", { clear = true }),
+  callback = function(ctx)
+    if not vim.tbl_contains(
+          { "javascript", "javascriptreact", "javascript.jsx", "typescript", "typescriptreact", "typescript.tsx", "vue" },
+          ctx.match
+        ) then
+      return
+    end
+
+    -- node
+    if vim.fn.findfile("package.json", ".;") ~= "" then
+      vim.lsp.enable("vtsls")
+      return
+    end
+
+    -- deno
+    vim.lsp.enable("denols")
+  end,
+})

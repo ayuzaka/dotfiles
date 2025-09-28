36a2def9750b60c7622fdb446647e18462d47dbc
ayuzaka <40567261+ayuzaka@users.noreply.github.com>
2025-09-28 22:02:24 +0900

    chore: font の管理方法の変更

---
+++ dot_config/wezterm/font_list.lua
@@ -0,0 +1,5 @@
+return {
+  { family = "PlemolJP35 Console", weight = "Regular" },
+  { family = "UDEV Gothic nf", weight = "Regular" },
+  { family = "Moralerspace Argon NF", weight = "Regular" },
+}
--- dot_config/wezterm/wezterm.lua
+++ dot_config/wezterm/wezterm.lua
@@ -1,12 +1,19 @@
-local wezterm = require 'wezterm'
-local keybinds = require('keybinds')
+local wezterm = require "wezterm"
+local keybinds = require("keybinds")
+local font_list = require "font_list"
+
+local resolve_font = function()
+  local ok, font_local = pcall(dofile, wezterm.config_dir .. "/font_local.lua")
+  if ok and type(font_local) == "table" then
+    return font_local
+  end
+
+  return font_list
+end
 
 return {
   color_scheme = "GruvboxDark",
-  font = wezterm.font_with_fallback {
-    { family = "UDEV Gothic nf", weight = 'Regular' },
-    { family = "Moralerspace Argon NF", weight = 'Regular' },
-  },
+  font = wezterm.font_with_fallback(resolve_font()),
   use_ime = true,
   macos_forward_to_ime_modifier_mask = "SHIFT|CTRL",
   hide_tab_bar_if_only_one_tab = true,
@@ -14,7 +21,7 @@ return {
   show_new_tab_button_in_tab_bar = false,
   window_decorations = "RESIZE",
   disable_default_key_bindings = true,
-  audible_bell = 'SystemBeep',
+  audible_bell = "SystemBeep",
   leader = keybinds.leader,
   keys = keybinds.keys,
 }

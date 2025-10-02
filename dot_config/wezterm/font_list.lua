00aa64f01685e9f77c59fc6953d79dfa38fa46a5
ayuzaka <40567261+ayuzaka@users.noreply.github.com>
2025-10-02 20:17:39 +0900

    chore: add font list

--- dot_config/wezterm/font_list.lua
+++ dot_config/wezterm/font_list.lua
@@ -2,4 +2,6 @@ return {
   { family = "PlemolJP35 Console",    weight = "Regular" },
   { family = "UDEV Gothic nf",        weight = "Regular" },
   { family = "Moralerspace Argon NF", weight = "Regular" },
+  { family = "firge", weight = "Regular" },
+  { family = "HackGen Console NF", weight = "Regular" },
 }

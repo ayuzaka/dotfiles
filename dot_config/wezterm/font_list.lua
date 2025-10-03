9627105ad173ba5986a5e6bcfae0559a3c2d37a7
kazuya <kazuya@melty.co.jp>
2025-10-03 10:26:09 +0900

    chore: add font explex

--- dot_config/wezterm/font_list.lua
+++ dot_config/wezterm/font_list.lua
@@ -1,5 +1,7 @@
 return {
   { family = "PlemolJP35 Console",    weight = "Regular" },
+  { family = "Explex Console NF", weight = "Regular" },
+  { family = "Explex35 Console NF", weight = "Regular" },
   { family = "UDEV Gothic nf",        weight = "Regular" },
   { family = "Moralerspace Argon NF", weight = "Regular" },
   { family = "firge", weight = "Regular" },

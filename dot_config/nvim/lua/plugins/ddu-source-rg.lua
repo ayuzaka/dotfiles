a3096b0e6808ef2880239ee64c5c5daa502aba78
ayuzaka <40567261+ayuzaka@users.noreply.github.com>
2025-10-25 10:41:45 +0900

    feat: Add a command to grep files under the directory specified by ddu-source-rg.

--- dot_config/nvim/lua/plugins/ddu-source-rg.lua
+++ dot_config/nvim/lua/plugins/ddu-source-rg.lua
@@ -14,6 +14,29 @@ vim.api.nvim_create_user_command("Grep", function()
   {}
 )
 
+vim.api.nvim_create_user_command("GrepDir", function()
+    local word = vim.fn.input("Search word: ")
+    local directory = vim.fn.input("Search directory (empty for current directory): ", "", "dir")
+    local params = {
+      input = word
+    }
+    if directory ~= nil and directory ~= "" then
+      params.paths = {
+        directory
+      }
+    end
+    vim.fn["ddu#start"]({
+      sources = {
+        {
+          name = "rg",
+          params = params
+        }
+      }
+    })
+  end,
+  {}
+)
+
 vim.api.nvim_create_user_command("GrepIgnore", function()
     vim.fn["ddu#start"]({
       sources = {

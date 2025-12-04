eb1257645eb8e18e3925506c7a706056231fa3b1
ayuzaka <40567261+ayuzaka@users.noreply.github.com>
2025-12-04 09:47:17 +0900

    chore: add keymap

--- dot_config/nvim/lua/config/keymaps.lua
+++ dot_config/nvim/lua/config/keymaps.lua
@@ -41,3 +41,6 @@ vim.api.nvim_set_keymap('', '<D-v>', '+p<CR>', { noremap = true, silent = true})
 vim.api.nvim_set_keymap('!', '<D-v>', '<C-R>+', { noremap = true, silent = true})
 vim.api.nvim_set_keymap('t', '<D-v>', '<C-R>+', { noremap = true, silent = true})
 vim.api.nvim_set_keymap('v', '<D-v>', '<C-R>+', { noremap = true, silent = true})
+
+vim.keymap.set("n", "<leader>gs", ":GitStatus<CR>", { silent = true })
+vim.keymap.set("n", "<leader>e", ":Filer<CR>", { silent = true })

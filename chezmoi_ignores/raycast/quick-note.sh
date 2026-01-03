a350755835bd2228ac8a32b14d699d082c8d8a90
ayuzaka <40567261+ayuzaka@users.noreply.github.com>
2026-01-03 15:19:02 +0900

    chore: use Ghostty

--- chezmoi_ignores/raycast/quick-note.sh
+++ chezmoi_ignores/raycast/quick-note.sh
@@ -13,7 +13,6 @@
 # @raycast.author ayuzaka
 # @raycast.authorURL https://raycast.com/ayuzaka
 
-# Alacritty を新規起動して nvim を開く
 tmp_dir="${TMPDIR:-/tmp}"
 tmp_file="${tmp_dir%/}/raycast-note-$(date +%Y%m%d%H%M%S).md"
 
@@ -23,24 +22,14 @@ initial_hash="$(shasum -a 256 "$tmp_file" | awk '{print $1}')"
 
 # 起動前に前面アプリを記録（nvim 終了後に戻してペーストするため）
 front_app="$(osascript -e 'tell application "System Events" to get name of first application process whose frontmost is true')"
-tmp_script="${tmp_dir%/}/raycast-note-$(date +%Y%m%d%H%M%S).sh"
 
-# Alacritty から実行する一時スクリプトを作成
-cat > "$tmp_script" <<EOF
-#!/bin/bash
-
-# nvim を開いて編集
-nvim "$tmp_file"
-
-# 変更があり、かつ空でないときだけクリップボードへコピー&元アプリにペースト
-final_hash="\$(shasum -a 256 "$tmp_file" | awk '{print \$1}')"
-if [ "\$final_hash" != "$initial_hash" ] && [ -s "$tmp_file" ]; then
-  cat "$tmp_file" | pbcopy
-  osascript -e 'tell application "$front_app" to activate'
-  osascript -e 'tell application "System Events" to keystroke "v" using command down'
+# Ghostty を新規起動して nvim を開く
+/Applications/Ghostty.app/Contents/MacOS/ghostty -e zsh -i -c "
+nvim '$tmp_file'
+final_hash=\"\$(shasum -a 256 '$tmp_file' | awk '{print \$1}')\"
+if [ \"\$final_hash\" != '$initial_hash' ] && [ -s '$tmp_file' ]; then
+  cat '$tmp_file' | pbcopy
+  osascript -e 'tell application \"$front_app\" to activate'
+  osascript -e 'tell application \"System Events\" to keystroke \"v\" using command down'
 fi
-EOF
-
-chmod +x "$tmp_script"
-
-alacritty -e zsh -i -c "$tmp_script"
+"

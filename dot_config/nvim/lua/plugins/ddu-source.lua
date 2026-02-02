b62a51d0519105bc08328b3ad3b91d439acbb51f
kazuya <kazuya@melty.co.jp>
2026-02-02 16:40:59 +0900

    chore: remove unused

--- dot_config/nvim/lua/plugins/ddu-source.lua
+++ dot_config/nvim/lua/plugins/ddu-source.lua
@@ -1,7 +1,3 @@
 vim.keymap.set("n", "<C-p>", function()
   vim.fn["ddu#start"]({ name = "fd" })
 end, { silent = true })
-
-vim.api.nvim_create_user_command("PRDiffFile", function()
-  vim.fn["ddu#start"]({ name = "gh_pr_diff_file" })
-end, {})

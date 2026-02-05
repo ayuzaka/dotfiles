7560d705225ea4f9492c1d6802d3c5cd4704692e
ayuzaka <40567261+ayuzaka@users.noreply.github.com>
2026-02-05 09:34:01 +0900

    style: format

--- dot_config/nvim/lua/config/plugins.lua
+++ dot_config/nvim/lua/config/plugins.lua
@@ -8,90 +8,128 @@ vim.opt.rtp:prepend(lazypath)
 
 require("lazy").setup({
   -- Immediate loading (required for first render)
-  { "morhetz/gruvbox", lazy = false, priority = 1000,
-    config = function() require("plugins.gruvbox") end },
+  {
+    "morhetz/gruvbox",
+    lazy = false,
+    priority = 1000,
+    config = function() require("plugins.gruvbox") end
+  },
   { "neovim/nvim-lspconfig", lazy = false },
-  { "tani/vim-artemis", lazy = false },
+  { "tani/vim-artemis",      lazy = false },
 
   -- BufReadPost / VeryLazy (on edit start)
-  { "nvim-treesitter/nvim-treesitter", branch = "main", event = "BufReadPost",
-    config = function() require("plugins.nvim-treesitter") end },
-  { "lewis6991/gitsigns.nvim", event = "BufReadPost",
-    config = function() require("plugins.gitsigns") end },
-  { "nvim-lualine/lualine.nvim", event = "VeryLazy",
-    config = function() require("plugins.lualine") end },
-  { "andymass/vim-matchup", event = "BufReadPost" },
+  {
+    "nvim-treesitter/nvim-treesitter",
+    branch = "main",
+    event = "BufReadPost",
+    config = function() require("plugins.nvim-treesitter") end
+  },
+  {
+    "lewis6991/gitsigns.nvim",
+    event = "BufReadPost",
+    config = function() require("plugins.gitsigns") end
+  },
+  {
+    "nvim-lualine/lualine.nvim",
+    event = "VeryLazy",
+    config = function() require("plugins.lualine") end
+  },
+  { "andymass/vim-matchup",      event = "BufReadPost" },
 
-  { "johmsalas/text-case.nvim", event = "BufReadPost",
-    config = function() require("plugins.text-case") end },
-  { "Bakudankun/BackAndForward.vim", event = "BufReadPost",
-    config = function() require("plugins.BackAndForward") end },
-  { "machakann/vim-sandwich", event = "BufReadPost" },
+  {
+    "johmsalas/text-case.nvim",
+    event = "BufReadPost",
+    config = function() require("plugins.text-case") end
+  },
+  {
+    "Bakudankun/BackAndForward.vim",
+    event = "BufReadPost",
+    config = function() require("plugins.BackAndForward") end
+  },
+  { "machakann/vim-sandwich",    event = "BufReadPost" },
   { "easymotion/vim-easymotion", event = "BufReadPost" },
-  { "bullets-vim/bullets.vim", event = "BufReadPost" },
-  { "cohama/lexima.vim", event = "InsertEnter" },
-  { "folke/ts-comments.nvim", event = "VeryLazy" },
-  { "cocopon/iceberg.vim", event = "VeryLazy" },
+  { "bullets-vim/bullets.vim",   event = "BufReadPost" },
+  { "cohama/lexima.vim",         event = "InsertEnter" },
+  { "folke/ts-comments.nvim",    event = "VeryLazy" },
+  { "cocopon/iceberg.vim",       event = "VeryLazy" },
 
   -- Command / FileType
-  { "nvim-lua/plenary.nvim", event = "VeryLazy" },
-  { "nvim-telescope/telescope.nvim", cmd = "Telescope",
-    config = function() require("plugins.telescope") end },
-  { "sbdchd/neoformat", cmd = { "Neoformat", "Prettier", "BiomeFormat", "FixJson", "SQLFormatter" },
+  { "nvim-lua/plenary.nvim",     event = "VeryLazy" },
+  {
+    "nvim-telescope/telescope.nvim",
+    cmd = "Telescope",
+    config = function() require("plugins.telescope") end
+  },
+  {
+    "sbdchd/neoformat",
+    cmd = { "Neoformat", "Prettier", "BiomeFormat", "FixJson", "SQLFormatter" },
     config = function()
       vim.api.nvim_create_user_command("Prettier", "Neoformat prettier", {})
       vim.api.nvim_create_user_command("BiomeFormat", "Neoformat biome", {})
       vim.api.nvim_create_user_command("FixJson", "Neoformat fixjson", {})
       vim.api.nvim_create_user_command("SQLFormatter", "Neoformat sqlformatter", {})
-    end },
-  { "nanotee/sqls.nvim", ft = "sql",
-    config = function() require("plugins.sqls") end },
-  { "mattn/emmet-vim", ft = { "html", "css", "vue", "svelte", "javascriptreact", "typescriptreact" } },
+    end
+  },
+  {
+    "nanotee/sqls.nvim",
+    ft = "sql",
+    config = function() require("plugins.sqls") end
+  },
+  { "mattn/emmet-vim",        ft = { "html", "css", "vue", "svelte", "javascriptreact", "typescriptreact" } },
   { "godlygeek/tabular" },
   { "preservim/vim-markdown", ft = "markdown" },
-  { "imsnif/kdl.vim", ft = "kdl" },
-  { "3rd/image.nvim", event = "VeryLazy",
-    config = function() require("plugins.image") end },
-  { "barrett-ruth/import-cost.nvim", build = "sh install.sh npm",
+  { "imsnif/kdl.vim",         ft = "kdl" },
+  {
+    "3rd/image.nvim",
+    event = "VeryLazy",
+    config = function() require("plugins.image") end
+  },
+  {
+    "barrett-ruth/import-cost.nvim",
+    build = "sh install.sh npm",
     ft = { "javascript", "typescript", "javascriptreact", "typescriptreact" },
-    config = function() require("plugins.import-cost") end },
+    config = function() require("plugins.import-cost") end
+  },
 
   -- Test runner
-  { "nvim-neotest/nvim-nio", event = "VeryLazy" },
-  { "nvim-neotest/neotest", event = "VeryLazy",
-    config = function() require("plugins.neotest") end },
-  { "marilari88/neotest-vitest", event = "VeryLazy" },
+  { "nvim-neotest/nvim-nio",               event = "VeryLazy" },
+  {
+    "nvim-neotest/neotest",
+    event = "VeryLazy",
+    config = function() require("plugins.neotest") end
+  },
+  { "marilari88/neotest-vitest",           event = "VeryLazy" },
 
   -- denops ecosystem (deferred Deno runtime startup)
-  { "vim-denops/denops.vim", event = "VeryLazy" },
-  { "Shougo/pum.vim", event = "VeryLazy" },
-  { "Shougo/ddc.vim", event = "VeryLazy" },
-  { "Shougo/ddc-ui-native", event = "VeryLazy" },
-  { "Shougo/ddc-ui-pum", event = "VeryLazy" },
-  { "Shougo/ddc-matcher_head", event = "VeryLazy" },
-  { "Shougo/ddc-sorter_rank", event = "VeryLazy" },
-  { "Shougo/ddc-source-lsp", event = "VeryLazy" },
-  { "LumaKernel/ddc-source-file", event = "VeryLazy" },
-  { "tani/ddc-fuzzy", event = "VeryLazy" },
-  { "vim-skk/skkeleton", event = "VeryLazy" },
-  { "Shougo/ddu.vim", event = "VeryLazy" },
-  { "Shougo/ddu-ui-ff", event = "VeryLazy" },
-  { "Shougo/ddu-source-file_rec", event = "VeryLazy" },
-  { "matsui54/ddu-source-file_external", event = "VeryLazy" },
-  { "uga-rosa/ddu-source-lsp", event = "VeryLazy" },
-  { "shun/ddu-source-rg", event = "VeryLazy" },
-  { "shun/ddu-source-buffer", event = "VeryLazy" },
-  { "Shougo/ddu-source-file", event = "VeryLazy" },
-  { "Shougo/ddu-kind-file", event = "VeryLazy" },
+  { "vim-denops/denops.vim",               event = "VeryLazy" },
+  { "Shougo/pum.vim",                      event = "VeryLazy" },
+  { "Shougo/ddc.vim",                      event = "VeryLazy" },
+  { "Shougo/ddc-ui-native",                event = "VeryLazy" },
+  { "Shougo/ddc-ui-pum",                   event = "VeryLazy" },
+  { "Shougo/ddc-matcher_head",             event = "VeryLazy" },
+  { "Shougo/ddc-sorter_rank",              event = "VeryLazy" },
+  { "Shougo/ddc-source-lsp",               event = "VeryLazy" },
+  { "LumaKernel/ddc-source-file",          event = "VeryLazy" },
+  { "tani/ddc-fuzzy",                      event = "VeryLazy" },
+  { "vim-skk/skkeleton",                   event = "VeryLazy" },
+  { "Shougo/ddu.vim",                      event = "VeryLazy" },
+  { "Shougo/ddu-ui-ff",                    event = "VeryLazy" },
+  { "Shougo/ddu-source-file_rec",          event = "VeryLazy" },
+  { "matsui54/ddu-source-file_external",   event = "VeryLazy" },
+  { "uga-rosa/ddu-source-lsp",             event = "VeryLazy" },
+  { "shun/ddu-source-rg",                  event = "VeryLazy" },
+  { "shun/ddu-source-buffer",              event = "VeryLazy" },
+  { "Shougo/ddu-source-file",              event = "VeryLazy" },
+  { "Shougo/ddu-kind-file",                event = "VeryLazy" },
   { "Shougo/ddu-filter-matcher_substring", event = "VeryLazy" },
-  { "matsui54/ddu-source-help", event = "VeryLazy" },
-  { "kuuote/ddu-source-git_status", event = "VeryLazy" },
-  { "kyoh86/ddu-source-git_log", event = "VeryLazy" },
-  { "ayuzaka/ddu-source-gh_pr_diff", event = "VeryLazy" },
-  { "ayuzaka/ddu-source-marks", event = "VeryLazy" },
-  { "Shougo/ddu-ui-filer", event = "VeryLazy" },
-  { "ryota2357/ddu-column-icon_filename", event = "VeryLazy" },
-  { "Shougo/ddu-column-filename", event = "VeryLazy" },
+  { "matsui54/ddu-source-help",            event = "VeryLazy" },
+  { "kuuote/ddu-source-git_status",        event = "VeryLazy" },
+  { "kyoh86/ddu-source-git_log",           event = "VeryLazy" },
+  { "ayuzaka/ddu-source-gh_pr_diff",       event = "VeryLazy" },
+  { "ayuzaka/ddu-source-marks",            event = "VeryLazy" },
+  { "Shougo/ddu-ui-filer",                 event = "VeryLazy" },
+  { "ryota2357/ddu-column-icon_filename",  event = "VeryLazy" },
+  { "Shougo/ddu-column-filename",          event = "VeryLazy" },
 })
 
 -- Immediate config

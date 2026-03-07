22e726a52b9cbcfed448c16d2e9aa9a808383548
ayuzaka <40567261+ayuzaka@users.noreply.github.com>
2026-03-07 22:11:17 +0900

    chore: update npm search

--- dot_config/nvim/lua/config/commands/npm_search.lua
+++ dot_config/nvim/lua/config/commands/npm_search.lua
@@ -6,17 +6,24 @@ local function url_encode(value)
   end))
 end
 
-local function npm_search()
-  local line = vim.api.nvim_get_current_line()
-  local col = vim.api.nvim_win_get_cursor(0)[2] + 1
-  local quoted_text = utils.get_quoted_text_under_cursor(line, col)
-  if not quoted_text then
-    vim.notify("Could not find quoted text under cursor", vim.log.levels.WARN)
+local function npm_search(opts)
+  local search_query = ""
+
+  if opts.range > 0 then
+    search_query = utils.get_visual_query_text()
+  else
+    local line = vim.api.nvim_get_current_line()
+    local col = vim.api.nvim_win_get_cursor(0)[2] + 1
+    search_query = utils.get_quoted_text_under_cursor(line, col)
+  end
+
+  if not search_query or search_query == "" then
+    vim.notify("Could not determine npm search query", vim.log.levels.WARN)
     return
   end
 
-  local url = "https://npmx.dev/" .. url_encode(quoted_text)
+  local url = "https://npmx.dev/" .. url_encode(search_query)
   vim.ui.open(url)
 end
 
-vim.api.nvim_create_user_command("NpmSearch", npm_search, {})
+vim.api.nvim_create_user_command("NpmSearch", npm_search, { range = true })

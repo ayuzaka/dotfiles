efdaa588238b7f43363864aa6fbfd3429457598c
ayuzaka <40567261+ayuzaka@users.noreply.github.com>
2026-03-07 11:20:24 +0900

    chore: add FileHistory keymap

--- dot_config/nvim/lua/config/keymaps.lua
+++ dot_config/nvim/lua/config/keymaps.lua
@@ -43,6 +43,7 @@ vim.api.nvim_set_keymap('t', '<D-v>', '<C-R>+', { noremap = true, silent = true}
 vim.api.nvim_set_keymap('v', '<D-v>', '<C-R>+', { noremap = true, silent = true})
 
 vim.keymap.set("n", "<leader>gs", ":GitStatus<CR>", { silent = true })
+vim.keymap.set("n", "<leader>gh", ":FileHistory<CR>", { silent = true })
 vim.keymap.set("n", "<leader>e", ":Filer<CR>", { silent = true })
 
 -- editprompt

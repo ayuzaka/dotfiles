117707cd0f992438e79cb3341457094b4696cbaf
ayuzaka <40567261+ayuzaka@users.noreply.github.com>
2026-03-27 23:48:06 +0900

    refactor: Buffer sending commands are clarified for direct app targets

---
+++ dot_config/nvim/lua/config/commands/send_buffer.lua
@@ -0,0 +1,36 @@
+local function send_to_app(target_app)
+  -- バッファ内容を取得
+  local lines = vim.api.nvim_buf_get_lines(0, 0, -1, false)
+  local content = table.concat(lines, "\n")
+  if content:match("^%s*$") then
+    vim.notify("Buffer is empty", vim.log.levels.WARN)
+    return
+  end
+
+  -- クリップボードにコピー
+  vim.fn.setreg("+", content)
+
+  -- ターゲットアプリをアクティブ化してペースト
+  vim.fn.system({ "osascript", "-e", string.format('tell application "%s" to activate', target_app) })
+  vim.fn.system({ "osascript", "-e", "delay 0.1", "-e", 'tell application "System Events" to keystroke "v" using command down' })
+
+  -- バッファをクリア
+  vim.api.nvim_buf_set_lines(0, 0, -1, false, {})
+  vim.notify("Sent to " .. target_app, vim.log.levels.INFO)
+end
+
+vim.api.nvim_create_user_command("SendTo", function(opts)
+  send_to_app(opts.args)
+end, { nargs = 1, desc = "Send buffer to specified app" })
+
+vim.api.nvim_create_user_command("SendToChrome", function()
+  send_to_app("Google Chrome Canary")
+end, { desc = "Send buffer to Google Chrome Canary" })
+
+vim.api.nvim_create_user_command("SendToSlack", function()
+  send_to_app("Slack")
+end, { desc = "Send buffer to Slack" })
+
+vim.keymap.set("n", "<leader>sc", "<cmd>SendToChrome<cr>", { desc = "Send buffer to Chrome Canary" })
+vim.keymap.set("n", "<leader>ss", "<cmd>SendToSlack<cr>", { desc = "Send buffer to Slack" })
+

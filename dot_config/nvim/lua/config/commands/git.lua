28d61c423d96b4f38ef3b340861b2908bd3ce8eb
ayuzaka <40567261+ayuzaka@users.noreply.github.com>
2026-04-02 09:11:24 +0900

    chore: fix open pr command

--- dot_config/nvim/lua/config/commands/git.lua
+++ dot_config/nvim/lua/config/commands/git.lua
@@ -1,8 +1,3 @@
-local function open_pr_by_hash(args)
-  local hash = args.args
-  vim.fn.system("git rev-parse " .. hash .. " | xargs gh openpr")
-end
-
 local function run_open_pr_by_hash()
   local word = vim.fn.expand("<cword>")
   if word == "" then
@@ -10,24 +5,11 @@ local function run_open_pr_by_hash()
     return
   end
 
-  open_pr_by_hash({ args = word })
+  local hash = word
+  vim.fn.system("git rev-parse " .. hash .. " | xargs gh openpr")
 end
 
-local function get_current_latest_blame_hash()
-  local file = vim.fn.expand("%:p")
-  local line = vim.fn.line(".")
-  local blame = vim.fn.system(string.format("git blame -L %d,%d %s", line, line, file))
-
-  return vim.split(blame, "\n")[1]:match("^(%x+)")
-end
-
-local function run_open_pr_current_line()
-  local hash = get_current_latest_blame_hash()
-  open_pr_by_hash({ args = hash })
-end
-
-vim.api.nvim_create_user_command("OpenPRCurrentLine", run_open_pr_current_line, {})
-vim.api.nvim_create_user_command("OpenPR", run_open_pr_by_hash, {})
+vim.api.nvim_create_user_command("PRView", run_open_pr_by_hash, {})
 
 local function gh_browse_current()
   local dir_path = vim.fn.fnamemodify(vim.fn.expand("%"), ":~:.")

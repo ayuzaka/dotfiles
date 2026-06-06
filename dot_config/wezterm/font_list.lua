a4aeaec79c45cc571cc88ac993d35e240f5150d9
ayuzaka <40567261+ayuzaka@users.noreply.github.com>
2026-06-06 15:22:39 +0900

    chore: update font

--- dot_config/wezterm/font_list.lua
+++ dot_config/wezterm/font_list.lua
@@ -1,10 +1,7 @@
 return {
   font = {
     { family = "Berkeley Mono", weight = "Regular" },
-    { family = "HackGen Console NF",    weight = "Regular" },
-    { family = "UDEV Gothic nf",        weight = "Regular" },
-    { family = "PlemolJP35 Console",    weight = "Regular" },
-    { family = "Moralerspace Argon NF", weight = "Regular" },
+    { family = "Kosugi", weight = "Regular" },
   },
   font_size = 14.0,
 }

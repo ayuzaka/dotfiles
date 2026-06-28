839c810bcd040b49676ec316e0c5db555ff0fecf
ayuzaka <40567261+ayuzaka@users.noreply.github.com>
2026-06-28 13:46:31 +0900

    chore: add abbrev with headroom

--- dot_config/zsh/abbrev.zsh
+++ dot_config/zsh/abbrev.zsh
@@ -3,7 +3,6 @@ typeset -gA ZSH_ABBR_MAP
 ZSH_ABBR_MAP=(
   ll 'lsd -lh'
   la 'lsd -lah'
-  cl 'clear'
   dcon-exec 'devcontainer exec --workspace-folder . /bin/bash -i'
   golint 'golangci-lint'
   gu 'gitui'
@@ -26,6 +25,8 @@ ZSH_ABBR_MAP=(
   gsave 'git_quicksave'
   gh-pr-me 'gh pr list --author "@me" --state all'
   gh-pr-search "gh pr list --state all --json number,title --jq \".[] | [.number,.title] | @csv\" --search \"__CURSOR__\" | fzf | awk -F ',' '{print \$1}' | xargs gh browse"
+  cl 'ANTHROPIC_BASE_URL=http://localhost:8787 claude'
+  cx 'OPENAI_BASE_URL=http://localhost:8787 codex'
 )
 
 function _zle_abbr_expand() {

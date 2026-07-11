bf0641f24fe6c61568cf3f204880c0467bfccebe
ayuzaka <40567261+ayuzaka@users.noreply.github.com>
2026-07-11 22:45:06 +0900

    chore: fix mac_init.sh

--- chezmoi_ignores/mac_init.sh
+++ chezmoi_ignores/mac_init.sh
@@ -9,22 +9,8 @@ defaults write com.apple.screencapture location ~/Downloads
 # すべての拡張子のファイルを表示する
 defaults write NSGlobalDomain AppleShowAllExtensions -bool true
 
-# Finder のタイトルバーにフルパスを表示する
-defaults write com.apple.finder _FXShowPosixPathInTitle -bool true
-defaults write com.apple.dock mcx-expose-disabled -bool true
-defaults write com.apple.dashboard mcx-disabled -bool true
-
 # .DS_Store ファイルを作らない
 defaults write com.apple.desktopservices DSDontWriteNetworkStores -bool true
-
-# Dock を右側に配置
-defaults write com.apple.dock orientation -string right
-
-# Dock のサイズを小さくする
-defaults write com.apple.dock tilesize -int 16
-
-# Automatically hide and show the Dock
-defaults write com.apple.dock autohide -bool true
 
 # Dock からアプリを削除
 defaults write com.apple.dock persistent-apps -array
